90a35223108c40bacd70d91f8effdbaff9173f46
ziad-farg <148318897+ziad-farg@users.noreply.github.com>
2025-05-26 04:51:12 +0300

    refactor: update category form to use new select component (#8)
    Co-authored-by: ziad-farg <ziad_farg2011@gmail.com>

--- resources/views/dashboard/categories/_form.blade.php
+++ resources/views/dashboard/categories/_form.blade.php
@@ -2,12 +2,9 @@
 
     {{-- Name --}}
     <div class="mb-3">
- feature/category-component
+        feature/category-component
         <x-form.label id="name">Name</x-form.label>
         <x-form.input name="name" :value="$category->name" />
-        <label for="name" class="form-label">Name</label>
-        <input type="text" @class(['form-control', 'is-invalid' => $errors->has('name')]) id="name" name="name" aria-label="Name"
-            value="{{ old('name', $category->name) }}" required />
         @error('name')
             <small class="text-danger">{{ $message }}</small>
         @enderror
@@ -16,14 +13,7 @@
     {{-- Parent Category --}}
     <div class="mb-3">
         <x-form.label id="parent_id">Parent Category</x-form.label>
-        <select class="form-control" id="parent_id" name="parent_id" aria-label="Parent Category">
-            <option value="">Primary Category</option>
-            @foreach ($categories as $item)
-                <option value="{{ $item->id }}" @selected(old('parent_id', $category->parent_id) == $item->id)>
-                    {{ $item->name }}
-                </option>
-            @endforeach
-        </select>
+        <x-form.select name="parent_id" :items="$categories" :select="$category->parent_id" />
     </div>
 
     {{-- image --}}
