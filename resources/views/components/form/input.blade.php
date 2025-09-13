74a4a04728e5608309152ef7f59e041fd3fe8c66
ziad-farg <ziad.farg.dev@gmail.com>
2025-09-13 07:40:28 +0300

    fix: remove required attribute from input component for image acceptance

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,7 +1,7 @@
 @props(['type' => 'text', 'name', 'value' => ''])
 
 <input type="{{ $type }}" id="{{ $name }}" name="{{ $name }}" value="{{ old($name, $value) }}"
-    {{ $attributes->class(['form-control', 'is-invalid' => $errors->has($name)]) }} accept="image/*" required />
+    {{ $attributes->class(['form-control', 'is-invalid' => $errors->has($name)]) }} accept="image/*" />
 
 @error($name)
     <small class="text-danger">{{ $message }}</small>
